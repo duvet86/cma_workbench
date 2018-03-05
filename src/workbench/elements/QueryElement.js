1d180939ff0e1a41baf5a26a2611e951e9eeaadb
Luca <duvet86@gmail.com>
2018-03-05 17:57:33 +0800

    Working on elements.

--- src/workbench/elements/QueryElement.js
+++ src/workbench/elements/QueryElement.js
@@ -6,10 +6,13 @@ import { getElementId } from "workbench/utils";
 import { operatorsExtraInfo } from "sideBar/operators/operatorsData";
 
 import Avatar from "material-ui/Avatar";
-
+import List, { ListItem, ListItemIcon, ListItemText } from "material-ui/List";
+import Divider from "material-ui/Divider";
 import Typography from "material-ui/Typography";
 
-const styles = {
+import Settings from "material-ui-icons/Settings";
+
+const styles = theme => ({
   operatorContainer: {
     position: "absolute",
     display: "flex",
@@ -19,35 +22,78 @@ const styles = {
     borderRadius: 5,
     border: "1px solid #003b86",
     padding: 5,
-    width: 165
+    width: 165,
+    backgroundColor: theme.palette.background.paper
   },
-  title: {
+  titleContainer: {
+    width: "100%",
     display: "flex",
     alignItems: "center",
-    marginBottom: 15
+    marginBottom: 5
   },
   avatar: {
-    marginRight: 15
+    marginRight: 10,
+    width: 25,
+    height: 25
+  },
+  list: {
+    overflow: "auto",
+    maxHeight: 150,
+    padding: 0,
+    width: 150
+  },
+  listItem: {
+    padding: 0
+  },
+  itemIcon: {
+    width: 10,
+    height: 10,
+    marginRight: 5
+  },
+  primary: {
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis"
   }
-};
+});
 
-const QueryElement = ({ classes, elementId, elementLabel, x, y }) => (
+const QueryElement = ({ classes, columns, elementId, elementLabel, x, y }) => (
   <div
     id={getElementId(elementId)}
     className={classes.operatorContainer}
     style={{ left: x, top: y }}
   >
-    <div className={classes.title}>
+    <div className={classes.titleContainer}>
       <Avatar
         className={classes.avatar}
         style={{ backgroundColor: operatorsExtraInfo[1].backgroundColor }}
       >
         {React.createElement(operatorsExtraInfo[1].IconComponent)}
       </Avatar>
+      <Typography variant="subheading" noWrap>
+        {elementLabel}
+      </Typography>
     </div>
-    <Typography variant="subheading" gutterBottom noWrap>
-      {elementLabel}
-    </Typography>
+    <div>
+      <Typography variant="body2" noWrap>
+        Columns
+      </Typography>
+      <Divider />
+      <List className={classes.list}>
+        {columns.map(({ Label, ColumnName }) => (
+          <ListItem key={ColumnName} className={classes.listItem} dense>
+            <ListItemIcon className={classes.itemIcon}>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText
+              className={classes.listItem}
+              classes={{ primary: classes.primary }}
+              primary={Label}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </div>
   </div>
 );
 
